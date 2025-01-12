eead8783e8c180762e747471fcac9897cf13f134
Wagner-github <jjullien.contact@gmail.com>
2025-01-12 15:06:47 +0100

    Update gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -3,4 +3,4 @@ import google.generativeai as genai
 
 ma_cle_secrete = st.secrets["GEMINI_API_KEY"] #récuperation de la clé API
 genai.configure(api_key=ma_cle_secrete) # configuration de la clé API
-#model_api = genai.GenerativeModel('gemini-1.5-flash')
+model_api = genai.GenerativeModel('gemini-1.5-flash')
