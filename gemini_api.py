2bd559d12b02c35c9bcf435f1feda941479e1f31
Wagner-github <jjullien.contact@gmail.com>
2025-01-13 10:56:01 +0100

    correction orthographe gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
 
-ma_cle_secrete = st.secrets["GEMINI_API_KEY"] #récuperation de la clé API
-genai.configure(api_key=ma_cle_secrete) # configuration de la clé API
+ma_cle_secrete = st.secrets["GEMINI_API_KEY"] #Récupération de la clé API
+genai.configure(api_key=ma_cle_secrete) # Configuration de la clé API
 model_api = genai.GenerativeModel('gemini-1.5-flash')
